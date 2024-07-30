3aaa049e831c0714c42f09c1eee5917737f63219
PMOLIO <118817661+PMOLIO@users.noreply.github.com>
2024-07-30 21:39:18 +0530

    Add files via upload

---
+++ TodoForm.jsx
@@ -0,0 +1,41 @@
+import { ListItem, TextField} from "@mui/material";
+import TodoItem from "./TodoItem";
+import { useState } from "react";
+import Create from "@mui/icons-material/Create"
+import {InputAdornment} from "@mui/material";
+import {IconButton} from "@mui/material";
+
+export default function TodoForm({addTodo})
+{
+    const [text,setText]= useState("");
+    const handleChange=(evt) =>
+    {
+        setText(evt.target.value)
+    }
+    const handleSubmit= (evt) =>
+    {
+        evt.preventDefault();
+        addTodo(text);
+        setText("");
+
+    }
+    return(
+        <ListItem>
+            <form onSubmit={handleSubmit}>
+            <TextField id="outlined-basic" label="Add todo" variant="outlined" onChange={handleChange} value={text}
+                       InputProps={{endAdornment:(<InputAdornment position="end">
+                        <IconButton
+                          aria-label="create todo"
+                          edge="end"
+                          type="submit"
+                        >
+                          <Create/>
+                        </IconButton>
+                      </InputAdornment>
+                       ),
+                    }}
+                    />
+            </form>
+        </ListItem>
+    )
+}
